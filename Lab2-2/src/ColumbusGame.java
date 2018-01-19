65f0bbb8424a3b280bb5415282348e4944a5cfeb
ahinojo1 <ahinojo1@mail.depaul.edu>
2018-01-18 22:08:55 -0600

    adding classes to columbus lab

--- Lab2-2/src/ColumbusGame.java
+++ Lab2-2/src/ColumbusGame.java
@@ -2,13 +2,13 @@ import javafx.application.Application;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-public class Columbus extends Application{
+public class ColumbusGame extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		Pane map = new Pane();
-		//testing git repo
+		
 	}
 	
 	public static void main(String[] args) {
